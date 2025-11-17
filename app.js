db513a6e45ad54cab1a8d9f0609a56214353dbb0
damthichieu2005-star <damthichieu2005@gmail.com>
2025-11-18 06:30:15 +0700

    Modify product entries in app.js
    Updated product details and prices.

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 const products = [
-  {
-    name: "Acc VIP 1 - 1.2tr",
-    price: 1200000,
-    desc: "Level 86 - 753 skin súng - Nhiều skin nâng cấp",
-    img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0161.jpeg"
-  },
-  {
-    name: "Acc VIP 2 - 1.5tr",
-    price: 1500000,
-    desc: "Nhiều súng nâng cấp - đồ hiếm",
-    img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0162.jpeg"
-  }
-];
+{
+  name: "Acc VIP 1 - 1.2tr",
+  price: 1200000,
+  desc: "Level 86 - Full súng, nhiều skin VIP",
+  img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0161.jpeg"
+},
+{
+  name: "Acc VIP 2 - 1.2tr",
+  price: 1200000,
+  desc: "Level 86 - Full đồ, chỉ số cao",
+  img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0162.jpeg"
+}
