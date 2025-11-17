70d310c1d4caa12f7b8624a83a3c65c625895db2
damthichieu2005-star <damthichieu2005@gmail.com>
2025-11-18 06:59:27 +0700

    Refactor products data structure to a single object
    Removed the products array and replaced it with a single product object.

--- app.js
+++ app.js
@@ -1,14 +1,6 @@
-const products = [
-  {
-    name: "Acc VIP 1 - 1.2tr",
-    price: 1200000,
-    desc: "Level 86 - Full súng, nhiều skin VIP",
-    img: "https://ibb.co/5WWwKyrr"
-  },
-  {
-    name: "Acc VIP 2 - 1.2tr",
-    price: 1200000,
-    desc: "Level 86 - Full đồ, chỉ số cao",
-    img: "https://ibb.co/GvpjK055"
-  }
-];
+{
+  "name": "Acc VIP 1 - 1tr2",
+  "price": 1200000,
+  "desc": "Level 86 - Nhiều skin",
+  "img": "https://ibb.co/5WWwKyrr"
+}
