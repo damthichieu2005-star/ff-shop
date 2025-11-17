fbe9e58c07c476b1a11000dc9f8abb8e83846462
damthichieu2005-star <damthichieu2005@gmail.com>
2025-11-18 06:25:07 +0700

    Refactor product data structure to an array
    Replaced single product object with an array of products, adding a second product with details.

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
-{
-  "name": "Acc VIP 1 - 1.2tr",
-  "price": 1200000,
-  "desc": "Level 86 - Nhiều skin",
-  "img": "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0161.jpeg"
-}
+const products = [
+  {
+    name: "Acc VIP 1 - 1.2tr",
+    price: 1200000,
+    desc: "Level 86 - 753 skin súng - Nhiều skin nâng cấp",
+    img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0161.jpeg"
+  },
+  {
+    name: "Acc VIP 2 - 1.5tr",
+    price: 1500000,
+    desc: "Nhiều súng nâng cấp - đồ hiếm",
+    img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0162.jpeg"
+  }
+];
