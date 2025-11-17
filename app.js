7f11c9b268f5680f908731b8569eadb51db3744c
damthichieu2005-star <damthichieu2005@gmail.com>
2025-11-18 06:52:39 +0700

    Update image URLs for VIP accounts

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ const products = [
     name: "Acc VIP 1 - 1.2tr",
     price: 1200000,
     desc: "Level 86 - Full súng, nhiều skin VIP",
-    img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0161.jpeg"
+    img: "https://ibb.co/5WWwKyrr"
   },
   {
     name: "Acc VIP 2 - 1.2tr",
     price: 1200000,
     desc: "Level 86 - Full đồ, chỉ số cao",
-    img: "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0162.jpeg"
+    img: "https://ibb.co/GvpjK055"
   }
 ];
