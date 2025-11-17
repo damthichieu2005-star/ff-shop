1e0f4eeaf84f63c072e3e8c18eeb93531bdf0864
damthichieu2005-star <damthichieu2005@gmail.com>
2025-11-18 06:21:52 +0700

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,6 @@
-
+{
+  "name": "Acc VIP 1 - 1.2tr",
+  "price": 1200000,
+  "desc": "Level 86 - Nhiều skin",
+  "img": "https://raw.githubusercontent.com/damthichieu2005-star/ff-shop/refs/heads/main/images/IMG_0161.jpeg"
+}
